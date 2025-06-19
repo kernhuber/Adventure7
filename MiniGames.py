0ff1c9f6b4d447b0c38f8d0f1fc98f168c2925af
Chris Funke <chris@kernhuber.de>
2025-06-19 14:13:39 +0200

    Minigames collection - for Dog Fight. Two games:
    number beats number to its left
    
    Sum up

---
+++ MiniGames.py
@@ -0,0 +1,142 @@
+"""
+Minigames for situations where Dog and Players have to fight
+"""
+import random
+
+from Utils import tw_print
+from NPCPlayerState import DogFight
+
+
+
+class MiniGames:
+    def circle_fight(self) -> DogFight:
+        """
+        Minigame: does number provided by player beat number provided by dog?
+        * both numbers one from 1,2,3,4
+        * 4 beats 3, 3 beats 2, 2 beats 1, 1 beats 4
+        * All other combinations -> TIE
+        * Check if dog has won -> return WON
+        * Check if player has lost -> return LOST
+        :param p: Player input (number from 1,2,3,4)
+        :return: DogFight state (WON, LOST, TIE) from Dog's perspective
+        """
+        import random
+        d = random.randint(1,4)
+
+
+        tw_print(f"***{'#'*40}***")
+        tw_print("***Kampf mit dem Hund!***".center(40))
+        tw_print("Regeln:  4 schlägt 3, 3 schlägt 2, 2 schlägt 1, 1 schlägt 4 \n... alles andere: Unentschieden")
+
+        inp = ""
+        while inp not in ["1","2","3","4"]:
+            inp = input("Gib eine Zahl aus 1,2,3 ein: ")
+        p = int(inp.strip())
+        tw_print(f"Du hast: {p}")
+        tw_print(f"Hund hat: {d}")
+        #
+        # Modulo calc: scale 1,2,3 to 0,1,2
+        #
+        d=d-1
+        p=p-1
+        if (d+1)%3 == p:
+            tw_print("***Der Hund verliert den Kampf!***")
+            return DogFight.LOST
+        if (p+1)%3 == d:
+            tw_print("***Du verlierst den Kampf gegen den Hund!***")
+            return DogFight.WON
+        tw_print("***Unentschieden!***")
+        return DogFight.TIE
+
+    def sum_fight(self) -> DogFight:
+        """
+        Given are 10 numbers and a value "reach" the system randomly creates out of them.
+
+        The Goal of the game is to reach the "reach" value by mutually picking a value from
+        the remaining values of the inital list and throw it on a "stack" (sum).
+
+        A player who exactly reaches "reach" wins the game
+
+        A player who places the last possible number, wins the game (last possible number:
+        any other from the numbers left would overstep the "reach" limit)
+
+        :return: DogFight
+        """
+        stones = []
+        stack=[]
+        stack_sum = 0
+        for i in range(0,10):
+            stones.append(random.randint(1,10))
+        z=0
+        for i in stones:
+            z = z+i
+        reach = random.randint(1,z)
+        print("Münzwurf: wer fängt an - Spieler oder Hund?")
+        whosnext = random.choice([0,1])
+        if whosnext == 0:
+            print("Dog fängt an!")
+        else:
+            print("Spieler fängt an!")
+        while True:
+            print(f"Verfügbare Zahlen: {stones}")
+            print(f"Ziel: {reach}")
+            print(f"Stapel: {stack_sum}")
+            if whosnext == 0:
+                #
+                # Dog draws:
+                #
+
+                i = random.randrange(0,len(stones))
+                stack.append((stones[i],"Dog"))
+                stack_sum += stones[i]
+                print(f"** Dog wählt: {stones[i]}")
+                if stack_sum == reach:
+                    print("Dog hat den Zielwert genau getroffen und gewinnt damit!")
+                    return DogFight.WON
+                stones.remove(stones[i])
+            else:
+                #
+                # Player draws
+                #
+                inp = 9999
+                while inp not in stones:
+                    try:
+                        inp = int(input("Wähle eine Zahl aus den verfügbaren Zahlen: "))
+                    except ValueError:
+                        print("Ungültige Eingabe. Bitte eine ganze Zahl eingeben.")
+                        inp = 9999
+                stack.append((inp,"Spieler"))
+                stack_sum += inp
+                if stack_sum == reach:
+                    print("Spieler hat den Zielwert genau getroffen - Dog verliert das Spiel")
+                    return DogFight.LOST
+                stones.remove(inp)
+
+
+
+
+            #
+            # Check if it is still possible for any player to continue the game:
+            #
+            lastone = True
+            for i in stones:
+                if stack_sum+i <= reach:
+                    lastone = False
+                    break
+
+            #
+            # lastone: The game is over
+            #
+            if lastone:
+                print(f"*** Game over - any further number out of {stones} would overstep {reach}")
+                if whosnext == 0:
+                    return DogFight.WON
+                else:
+                    return DogFight.LOST
+
+            whosnext = (whosnext + 1) % 2
+            print("--------------")
+
+
+g = MiniGames()
+g.sum_fight()
