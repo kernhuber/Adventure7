00b4649d8a1d071945b6e74accf1a2d40c283c3e
Chris Funke <chris@kernhuber.de>
2025-07-10 18:08:11 +0200

    Some minor beautifications. Fixed an error in ExplosionState. Added game_won_text.

--- ExplosionState.py
+++ ExplosionState.py
@@ -10,7 +10,7 @@ class ExplosionState(PlayerState):
     """ NPC Player *Explosion* - lives for 3 rounds and eliminates things where it explodes, possibly triggering further actions"""
 
     kaboom_timer: int = 3    # Detonate in "koboom_timer" rounds
-    name: str
+    #name: str
 
     def explosion_input(self, gs:GameState) -> str:
         from Place import Place
