4ecf5794e7a4b5b0656fa57cd1eca81e25235e1a
Chris Funke <chris@kernhuber.de>
2025-07-10 13:41:57 +0200

    Some minor beautifications

--- start.sh
+++ start.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 git pull
+git checkout Adventure8-LLM
 test -e ./venv || ( python3 -m venv ./venv; . .venv/bin/activate; pip install -r requirements.txt )
 clear
 echo
 echo
 . ./venv/bin/activate
-. .env
-export GOOGLE_API_KEY
+
+if test "$GOOGLE_API_KEY" = ""; then
+  echo "GOOGLE_API_KEY ist nicht gesetzt. Der API-Key muss manuell eingegeben werden"
+else
+  export GOOGLE_API_KEY
+fi
 
 python3 Adventure8.py
 
