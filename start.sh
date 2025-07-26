b86bf149a47b094607ab8162e60c091317459314
Chris Funke <chris@kernhuber.de>
2025-07-26 23:13:08 +0200

    * Adjusted to new branch

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 git pull
 git checkout Adventure9-Zombie
 test -e ./venv || ( python3 -m venv ./venv; . ./venv/bin/activate; pip install -r requirements.txt )
+test -e ./.apikey && . ./.apikey
 clear
 echo
 echo
@@ -13,5 +14,5 @@ else
   export GOOGLE_API_KEY
 fi
 
-python3 Adventure9.py
+python3 ./web_backend_server.py
 
